d609d14595a340b47672c5a361ce5ed6b485150f
Adrian Fernandez <adrianfernandezalv@gmail.com>
2024-03-25 18:10:05 +0100

    added user to login response

--- src/main/java/com/gamedex/backend/user/UserEntity.java
+++ src/main/java/com/gamedex/backend/user/UserEntity.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gamedex.backend.user.enums.RoleEnum;
 
 import jakarta.validation.constraints.NotBlank;
@@ -55,16 +56,19 @@ public class UserEntity implements UserDetails {
         return List.of(new SimpleGrantedAuthority((role.name())));
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
@@ -73,6 +77,12 @@ public class UserEntity implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getPassword() {
+        return this.password;
     }
 
 }
