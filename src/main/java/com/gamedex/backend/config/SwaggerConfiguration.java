519612032ea139bf8889c61054983c038a2434df
Adrian Fernandez <adrianfernandezalv@gmail.com>
2024-03-25 19:42:53 +0100

    add local & prod environment for swagger

--- src/main/java/com/gamedex/backend/config/SwaggerConfiguration.java
+++ src/main/java/com/gamedex/backend/config/SwaggerConfiguration.java
@@ -1,5 +1,8 @@
 package com.gamedex.backend.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +11,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -16,15 +20,21 @@ public class SwaggerConfiguration {
     public OpenAPI openAPI() {
         return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth", createAPIKeyScheme()))
-                .info(new Info().title("Gamedex API")
-                        // .description("Some custom description of API.")
-                        .version("1.0"));
+                .servers(servers())
+                .info(new Info().title("Gamedex API").version("1.0"));
     }
 
     private SecurityScheme createAPIKeyScheme() {
         return new SecurityScheme().type(SecurityScheme.Type.HTTP)
                 .bearerFormat("JWT")
                 .scheme("bearer");
+    }
+
+    private List<Server> servers() {
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("http://localhost:8080").description("Local Environment"));
+        servers.add(new Server().url("https://api.example.com").description("Production Environment"));
+        return servers;
     }
 
 }
